99ed61607e580a00be839460ea721f637982f637
yurifmorais <yfmorais@inf.ufsm.com>
2023-08-21 09:11:23 -0300

    email unique

--- api/src/main/java/conecta/vagas/api/domain/user/UserDataRegister.java
+++ api/src/main/java/conecta/vagas/api/domain/user/UserDataRegister.java
@@ -2,6 +2,7 @@ package conecta.vagas.api.domain.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.UniqueElements;
 
 public record UserDataRegister(
         @NotBlank
