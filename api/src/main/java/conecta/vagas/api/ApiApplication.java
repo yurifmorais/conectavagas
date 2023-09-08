21b000b2ac9879a7047d546edb44a20fd03f5396
yurifmorais <yfmorais@inf.ufsm.com>
2023-09-08 15:24:01 -0300

    updated db and main

--- api/src/main/java/conecta/vagas/api/ApiApplication.java
+++ api/src/main/java/conecta/vagas/api/ApiApplication.java
@@ -3,7 +3,7 @@ package conecta.vagas.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//next step: configure infra files
+//next step: generate route to show db data in json format. 
 @SpringBootApplication
 public class ApiApplication {
 
--- api/src/main/resources/db/migration/V3__create-table-resumes.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE resumes (
-  id BIGINT PRIMARY KEY AUTO_INCREMENT,
-  user_id BIGINT NOT NULL,
-  experiences TEXT,
-  skills TEXT,
-  education TEXT,
-  contact_info TEXT,
-  FOREIGN KEY (user_id) REFERENCES users(id)
-);
